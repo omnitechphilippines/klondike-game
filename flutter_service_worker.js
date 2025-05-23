0390915ac26a0521a5ea78bde07d37e315daa448
omnitechphilippines <omnitechphilippines@users.noreply.github.com>
2025-05-23 07:45:59 +0000

    deploy: cb3fa3116fae964c44c823bfc04a57216228b5bf

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2e
 "canvaskit/chromium/canvaskit.js": "8191e843020c832c9cf8852a4b909d4c",
 "canvaskit/chromium/canvaskit.wasm": "c054c2c892172308ca5a0bd1d7a7754b",
 "canvaskit/chromium/canvaskit.js.symbols": "f7c5e5502d577306fb6d530b1864ff86",
-"flutter_bootstrap.js": "23ed03d5091b0ef3ccf91696d406fd36",
+"flutter_bootstrap.js": "f9fb21e0b5b61c81077f61c9017cb098",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "index.html": "231a8fd78c7da39854fb9ead94f2b58e",
 "/": "231a8fd78c7da39854fb9ead94f2b58e",
